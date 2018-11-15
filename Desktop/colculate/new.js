bd0442ced5ade24f3e57cc613d4eb112e7720b8c
Mher Arshakyan <mher.arshakyan@softconstruct.local>
2018-11-15 17:38:55 +0400

    new version

--- Desktop/colculate/new.js
+++ Desktop/colculate/new.js
@@ -1,3 +1,8 @@
+var firNumbers = 0;
+var operators;
+var elementBackColor;
+var x = 0;
+
 function disable() {
   document.onkeydown = function(e) {
     return false;
@@ -5,19 +10,42 @@ function disable() {
 }
 disable();
 
-function number(x) {
-  document.getElementById("numb").value += x;
+function operations( firstNumb, operator, element ) {
+  x++;
+  firNumbers = +firstNumb;
+  operators = operator;
+  elementBackColor = element;
+  document.getElementById( "numb" ).value = null;
+  element.style.backgroundColor = "#c6c6c6";
 };
-function reset(s) {
-  let y = s.substring(0, s.length - 1);
-  document.getElementById("numb").value = y;
+
+function number( x ) {
+  document.getElementById( "numb" ).value += x;
 };
-function result(x) {
-  console.log(typeof x);
-  const result = eval(x);
-  document.getElementById("numb").value = result;
-  return result;
+
+function reset( s ) {
+  let y = s.substring( 0, s.length - 1 );
+  document.getElementById( "numb" ).value = y;
 };
-function plus(){
-  alert("plus");
+
+function result( x ) {
+  let secNumbers = +x;
+  let resultat;
+ switch (operators) {
+   case "+":
+    resultat = firNumbers + secNumbers;
+     break;
+   case "-":
+    resultat = firNumbers - secNumbers;
+     break;
+   case "*":
+    resultat = firNumbers * secNumbers;
+     break;
+   case "/":
+    resultat = firNumbers / secNumbers;
+     break;
+   default:
+ }
+  document.getElementById( "numb" ).value = resultat;
+  elementBackColor.style.backgroundColor = "#f2f2f2";
 };
